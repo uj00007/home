9027386e3fb950dde85750fff1abafaa353f158d
Ujjwal Goyal <hasher@LAP-MACA-810.local>
2019-07-30 01:06:56 +0530

    added docs folder

---
+++ docs/precache-manifest.c3557cff3e3acd904a88186f10c3b789.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee22379c4d76452bee5c1abbeb69bcbb",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "c1fe96ec1395d2f64b7a",
+    "url": "/home/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/home/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c1fe96ec1395d2f64b7a",
+    "url": "/home/static/js/main.53742bbc.chunk.js"
+  },
+  {
+    "revision": "4e1ec8947a6857600002",
+    "url": "/home/static/js/runtime~main.0990ee84.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/home/static/media/logo.5d5d9eef.svg"
+  }
+]);
